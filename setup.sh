c9332d2939f091a544cc3f4c5867ce14cdafef94
Chris Patton <cd.patton@gmail.com>
2024-09-15 13:35:55 -0700

    fixed user profile issues?

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Update and upgrade the system
+# may need to make the file executable chmod +x setup.sh
+
+Update and upgrade the system
 sudo apt update && sudo apt upgrade -y
 
 # Install Google Chrome
@@ -9,7 +11,7 @@ sudo apt install ./google-chrome-stable_current_amd64.deb -y
 rm google-chrome-stable_current_amd64.deb
 
 # Install VS Code
-sudo apt install software-properties-common apt-transport-https wget -y
+# sudo apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
@@ -22,19 +24,21 @@ sudo apt install git -y
 git config --global user.name "Chris Patton"
 git config --global user.email "cd.patton@gmail.com"
 
-# Create repos folder in the current user's home directory
-mkdir -p "$HOME/repos"
+# Get the user's home directory
+USER_HOME=$(eval echo ~"$SUDO_USER")
 
-# Clone the crispyDyne/ubuntu_home repository into the repos folder
-git clone https://github.com/crispyDyne/work_setup.git "$HOME/repos/work_setup"
+# # Create repos folder in the current user's home directory
+# sudo -u "$SUDO_USER" mkdir -p "$USER_HOME/repos"
+
+# Clone the crispyDyne/work_setup repository into the repos folder
+sudo -u "$SUDO_USER" git clone https://github.com/crispyDyne/work_setup.git "$USER_HOME/repos/work_setup"
 
 # Copy VS Code settings from the repository to the local machine
-cp "$HOME/repos/work_setup/vscode/settings.json" "$HOME/.config/Code/User/settings.json"
+cp "$USER_HOME/repos/work_setup/vscode/settings.json" "$USER_HOME/.config/Code/User/settings.json"
 
-# Install desired VS Code extensions
-# sudo -u "$SUDO_USER" code --install-extension GitHub.copilot # specificly do not run as sudo. Do I need to?
-code --install-extension GitHub.copilot
-code --install-extension eamodio.gitlens
-code --install-extension streetsidesoftware.code-spell-checker
+# Install VS Code extensions
+for ext in GitHub.copilot eamodio.gitlens streetsidesoftware.code-spell-checker; do
+  sudo -u "$SUDO_USER" code --install-extension "$ext"
+done
 
 echo "Setup complete!"
